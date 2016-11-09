8807b3326c8266aa8d5e4c3ff4b82e5360c0dbe8
HimpeWillem <willem.himpe@gmail.com>
2016-11-09 15:14:55 +0100

    TF adjustement for multiple origin connectors
    This is a fix for a network with multiple origin connectors.
    No check has been made (all single origin connector networks still work)

--- Dynamic Traffic Assignment/ILTM_BASE.m
+++ Dynamic Traffic Assignment/ILTM_BASE.m
@@ -203,7 +203,7 @@ display(['total number of node updates: ',num2str(avg_it*length(normalNodes)+tot
                 for d_index = 1:totDest
                     %calculation sending flow
                     SF_d = TF{o,t-1,d_index}.*sum(ODmatrix(o_index,d_index,t-1))*dt;
-                    cvn_up(l,t,d_index)=cvn_up(l,t-1,d_index) + SF_d;
+                    cvn_up(l,t,d_index)=cvn_up(l,t-1,d_index) + SF_d(l_index);
                 end
             end
         end 
